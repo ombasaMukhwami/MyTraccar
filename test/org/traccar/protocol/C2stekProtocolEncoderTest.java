0ba3c1653d63c1acd687d66ead2ad4a258996147
ombasaMukhwami <mukhwami@gmail.com>
2019-05-28 09:29:20 +0000

    C2stekprotocolEncoder

---
+++ test/org/traccar/protocol/C2stekProtocolEncoderTest.java
@@ -0,0 +1,18 @@
+package org.traccar.protocol;
+
+
+import org.traccar.ProtocolTest;
+
+
+
+
+
+
+public class C2stekProtocolEncoderTest extends ProtocolTest {
+
+    private C2stekProtocolEncoder encoder = new C2stekProtocolEncoder();
+
+
+
+}
+
