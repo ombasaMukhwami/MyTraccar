8bd67d5c904cc59c61ecf27e0b54bae9cf2945ad
ombasaMukhwami <mukhwami@gmail.com>
2019-06-18 07:41:11 +0000

    Karibia and itrackafrica protocol implemented, NTSA

---
+++ src/org/traccar/protocol/KaribiaProtocol.java
@@ -0,0 +1,39 @@
+package org.traccar.protocol;
+
+import org.traccar.BaseProtocol;
+import org.traccar.PipelineBuilder;
+import org.traccar.TrackerServer;
+import org.traccar.model.Command;
+
+import io.netty.handler.codec.string.StringEncoder;
+
+public class KaribiaProtocol extends BaseProtocol {
+
+    public KaribiaProtocol() {
+        setSupportedDataCommands(
+                Command.TYPE_POSITION_SINGLE,
+                Command.TYPE_ENGINE_STOP,
+                Command.TYPE_ENGINE_RESUME,
+                Command.TYPE_IDENTIFICATION,
+                Command.TYPE_REBOOT_DEVICE);
+        addServer(new TrackerServer(false, getName()) {
+            @Override
+            protected void addProtocolHandlers(PipelineBuilder pipeline) {
+                pipeline.addLast(new Huabao1FrameDecoder());
+                pipeline.addLast(new StringEncoder());
+                pipeline.addLast(new HuabaoProtocolEncoder());
+                pipeline.addLast(new Huabao1ProtocolDecoder(KaribiaProtocol.this));
+            }
+        });
+        addServer(new TrackerServer(true, getName()) {
+            @Override
+            protected void addProtocolHandlers(PipelineBuilder pipeline) {
+                pipeline.addLast(new Huabao1FrameDecoder());
+                pipeline.addLast(new StringEncoder());
+                pipeline.addLast(new HuabaoProtocolEncoder());
+                pipeline.addLast(new Huabao1ProtocolDecoder(KaribiaProtocol.this));
+            }
+        });
+    }
+
+}
